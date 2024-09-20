a7407b1508e0c5aceefa3266b4185debe6aadd29
backcate <muhammad.de.home@gmail.com>
2024-09-20 23:39:12 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+function toggle() {
+    var q = document.getElementById("shows");
+    var s = document.getElementById("shows_1");
+    var w = document.getElementById("hiden");
+    var x = document.getElementById("hiden_1");
+    var y = document.getElementById("hiden_2");
+    var z = document.getElementById("hiden_3");
+    var t = document.getElementById("hiden_4");
+    
+
+    if (q.style.display === "flex") {
+    }
+    else {
+        q.style.display = "flex"
+    }
+
+    if (s.style.display === "flex") {
+    }
+    else {
+        s.style.display = "flex"
+    }
+
+    if (t.style.display === "none") {
+    }
+    else {
+        t.style.display = "none"
+    }
+
+    if (w.style.display === "none") {
+    }
+    else {
+        w.style.display = "none"
+    }
+
+    if (x.style.display === "none") {
+    }
+    else {
+        x.style.display = "none"
+    }
+    
+    if (y.style.display === "none") {
+    }
+    else {
+        y.style.display = "none"
+    }
+
+    if (z.style.display === "none") {
+    }
+    else {
+        z.style.display = "none"
+    }
+}
+
+function toggle2() {
+    var v = document.getElementById("shows");
+    var s = document.getElementById("shows_1");
+    var w = document.getElementById("hiden");
+    var x = document.getElementById("hiden_1");
+    var y = document.getElementById("hiden_2");
+    var z = document.getElementById("hiden_3");
+    var t = document.getElementById("hiden_4");
+    
+
+    if (v.style.display === "none") {
+    }
+    else {
+        v.style.display = "none"
+    }
+
+    if (w.style.display === "flex") {
+    }
+    else {
+        w.style.display = "flex"
+    }
+
+    if (s.style.display === "none") {
+    }
+    else {
+        s.style.display = "none"
+    }
+
+    if (x.style.display === "flex") {
+    }
+    else {
+        x.style.display = "flex"
+    }
+    
+    if (y.style.display === "flex") {
+    }
+    else {
+        y.style.display = "flex"
+    }
+
+    if (z.style.display === "flex") {
+    }
+    else {
+        z.style.display = "flex"
+        z.style.color = "red"
+    }
+
+    if (t.style.display === "flex") {
+    }
+    else {
+        t.style.display = "flex"
+    }
+}
